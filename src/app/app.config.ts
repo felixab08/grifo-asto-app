38fdf096a0285cffa83d25eb720f19afe6211ec9
Felix <gatoab2@gmail.com>
2025-11-20 21:43:52 -0500

    feat: HashLocationStrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import localEs from '@angular/common/locales/es';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy, registerLocaleData } from '@angular/common';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+registerLocaleData(localEs, 'es', 'es-ES');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-ES',
+    },
+    provideHttpClient(withFetch()),
+  ],
 };
