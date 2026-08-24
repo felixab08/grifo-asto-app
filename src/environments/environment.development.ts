df0c318d4b354d7b94b7ba6bf1cbddaf09a366d1
Felix <gatoab2@gmail.com>
2026-08-24 15:19:36 -0500

    feat: change service in back

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // baseUrl: 'http://localhost:8080/api',
-  baseUrl: 'http://192.168.100.4:8080/api',
+  baseUrl: 'https://grifo-asto-core-production.up.railway.app/api',
   // baseUrl: 'http://192.168.0.43:8080/api',
 };
